c42acecd7216f4d17469c42eaa64fcce1709e239
zahid hasan <zahidhasan6112@gmail.com>
2022-08-30 09:55:34 -0700

    homepage working

---
+++ index.php
@@ -0,0 +1,651 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+
+	<!--For responsive	-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lawyer</title>
+
+	<!--Bootstrap CSS	-->
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+	<!--Google Font	-->
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<!--Fontawesome icon	-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+
+
+	<!--OWl carousel	-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+
+
+	<!--Style CSS	-->
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+
+</head>
+
+<body>
+
+
+
+	<!-- Start Top Header -->
+	<div class="top-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="top-contact">
+						<ul>
+							<li><a href="tel:+62 800 9000 123"> <i class="fa fa-phone"></i> +62 800 9000 123</a> </li>
+							<li><a href="mailto:mail@yoursite.com"> <i class="fa fa-envelope"></i> mail@yoursite.com</a> </li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-6">
+					<div class="top-right-info">
+						<ul class="top-social">
+							<li><a href=""><i class="fa fa-facebook"></i></a> </li>
+							<li><a href=""><i class="fa fa-instagram"></i></a> </li>
+							<li><a href=""><i class="fa fa-twitter"></i></a> </li>
+							<li><a href=""><i class="fa fa-pinterest"></i></a> </li>
+						</ul>
+
+						<a href="" class="btn btn-primary">book an appointment</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- End Top Header -->
+
+
+	<!--Start Header	-->
+	<header>
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#"><img src="images/logo.png" alt=""></a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+							<li class="nav-item">
+								<a class="nav-link " href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#about-section-id">ABOUT US</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#services-section-id">SERVICES</a>
+							</li>
+
+							<li class="nav-item">
+								<a class="nav-link" href="#">NEWS</a>
+							</li>
+
+							<li class="nav-item">
+								<a class="nav-link" href="#contact-us-id">CONTACT</a>
+							</li>
+
+						</ul>
+
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--End Header	-->
+
+
+	<!--Start Banner Section	-->
+	<section class="banner-section" style="background: url(images/banner-bg.jpg) no-repeat">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="banner-text">
+						<h1>Welcome to Lawyer & Attorney</h1>
+						<p>We provide high standard clean website for your business solutions sit amet consectetuer.</p>
+						<div class="btn-main">
+							<a href="#team-section-id" class="btn btn-primary">Read More</a>
+							<a href="#quote-section-id" class="btn btn-outline-light">GET A QUOTE</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--End Banner Section	-->
+
+
+	<!--Start Features Section	-->
+	<section class="features-section">
+		<div class="container">
+			<div class="row top-features">
+
+				<div class="col-lg-4">
+					<div class="features-box">
+						<img src="images/icon.png" alt="">
+						<h3>Winning Award</h3>
+						<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+					</div>
+				</div>
+
+				<div class="col-lg-4">
+					<div class="features-box">
+						<img src="images/icon2.png" alt="">
+						<h3>Private</h3>
+						<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+					</div>
+				</div>
+
+
+
+				<div class="col-lg-4">
+					<div class="features-box">
+						<img src="images/icon3.png" alt="">
+						<h3>Legal Protection</h3>
+						<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row align-items-center">
+				<div class="col-lg-4">
+					<div class="left-head">
+						<h2>Best Law & Attorney for everyone</h2>
+					</div>
+				</div>
+
+				<div class="col-lg-8">
+					<div class="right-text">
+						<p>Lorem ipsum dolor sit amet con sectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+						<p>Lorem ipsum dolor sit amet con sectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+		</div>
+	</section>
+	<!--Start Features Section	-->
+
+
+
+
+	<!--Start About Section	-->
+	<section class="about-section" id="about-section-id">
+		<div class="container">
+			<div class="row  align-items-center">
+				<div class="col-lg-6">
+					<div class="about-text text-center">
+						<h2>About law & Attorney </h2>
+						<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium.
+						</p>
+						<div class="signature">
+							<img src="images/signature.png" alt="">
+							<p>CEO & Founder Of Tak Desain</p>
+						</div>
+
+					</div>
+				</div>
+
+
+				<div class="col-lg-6">
+					<div class="about-img">
+						<img src="images/about.jpg" alt="">
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!--End About Section	-->
+
+
+
+	<!--Start Services	-->
+	<section class="services-section" id="services-section-id">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12">
+					<div class="section-title-main">
+						<h2 class="section-title">Legal Practices Area</h2>
+					</div>
+				</div>
+
+
+				<div class="col-lg-4">
+					<div class="services-box">
+						<div class="services-media">
+							<img src="images/services1.jpg" alt="">
+						</div>
+						<div class="services-info">
+							<h3>Fire Accident</h3>
+							<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-lg-4">
+					<div class="services-box">
+						<div class="services-media">
+							<img src="images/services2.jpg" alt="">
+						</div>
+						<div class="services-info">
+							<h3>Accident Injuries</h3>
+							<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4">
+					<div class="services-box">
+						<div class="services-media">
+							<img src="images/services3.jpg" alt="">
+						</div>
+						<div class="services-info">
+							<h3>Family Law</h3>
+							<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-lg-4">
+					<div class="services-box">
+						<div class="services-media">
+							<img src="images/services4.jpg" alt="">
+						</div>
+						<div class="services-info">
+							<h3>Financial Law</h3>
+							<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-lg-4">
+					<div class="services-box">
+						<div class="services-media">
+							<img src="images/services5.jpg" alt="">
+						</div>
+						<div class="services-info">
+							<h3>Drug Offences</h3>
+							<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-lg-4">
+					<div class="services-box">
+						<div class="services-media">
+							<img src="images/services6.jpg" alt="">
+						</div>
+						<div class="services-info">
+							<h3>Civil Litigation</h3>
+							<p>sum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore .</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!--End Services	-->
+
+
+
+	<!--Start Team Section-->
+	<section class="team-section" id="team-section-id">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12">
+					<div class="section-title-main">
+						<h2 class="section-title">Meet Our Lawyers</h2>
+					</div>
+				</div>
+
+				<div class="col-lg-3">
+					<div class="team-box">
+						<div class="team-media">
+							<img src="images/team1.jpg" alt="">
+						</div>
+						<div class="team-info">
+							<h3>Rudhi Sasmito</h3>
+							<p>- Lead Lawyer -</p>
+							<ul class="top-social">
+								<li><a href=""><i class="fa fa-facebook"></i></a> </li>
+								<li><a href=""><i class="fa fa-instagram"></i></a> </li>
+								<li><a href=""><i class="fa fa-twitter"></i></a> </li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a> </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3">
+					<div class="team-box">
+						<div class="team-media">
+							<img src="images/team2.jpg" alt="">
+						</div>
+						<div class="team-info">
+							<h3>Rudhi Sasmito</h3>
+							<p>- Lead Lawyer -</p>
+							<ul class="top-social">
+								<li><a href=""><i class="fa fa-facebook"></i></a> </li>
+								<li><a href=""><i class="fa fa-instagram"></i></a> </li>
+								<li><a href=""><i class="fa fa-twitter"></i></a> </li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a> </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-lg-3">
+					<div class="team-box">
+						<div class="team-media">
+							<img src="images/team3.jpg" alt="">
+						</div>
+						<div class="team-info">
+							<h3>Sarah Doel</h3>
+							<p>- Lead Lawyer -</p>
+							<ul class="top-social">
+								<li><a href=""><i class="fa fa-facebook"></i></a> </li>
+								<li><a href=""><i class="fa fa-instagram"></i></a> </li>
+								<li><a href=""><i class="fa fa-twitter"></i></a> </li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a> </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3">
+					<div class="team-box">
+						<div class="team-media">
+							<img src="images/team4.jpg" alt="">
+						</div>
+						<div class="team-info">
+							<h3>Rome Doel</h3>
+							<p>- Lead Lawyer -</p>
+							<ul class="top-social">
+								<li><a href=""><i class="fa fa-facebook"></i></a> </li>
+								<li><a href=""><i class="fa fa-instagram"></i></a> </li>
+								<li><a href=""><i class="fa fa-twitter"></i></a> </li>
+								<li><a href=""><i class="fa fa-pinterest"></i></a> </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12">
+					<div class="btn-team text-center">
+						<a href="" class="btn btn-primary">All Attorney</a>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</section>
+	<!--End Team Section-->
+
+
+
+	<!--	Start Testimonial Section-->
+	<section class="quote-section" style="background: url(images/review-bg.jpg)" id="quote-section-id">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title-main">
+						<h2 class="section-title">OUR QUOTE</h2>
+					</div>
+
+					<div class="col-md-12">
+						<div class="owl-carousel owl-theme testimonial-carousel">
+							<div class="item">
+								<div class="test-box">
+									<p>“Consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ”</p>
+									<div class="test-info">
+										<img src="images/review-img.jpg" alt="">
+										<div class="test-authot">
+											<h4>Sonata River</h4>
+											<h5>Company ABC</h5>
+										</div>
+									</div>
+
+								</div>
+							</div>
+
+
+							<div class="item">
+								<div class="test-box">
+									<p>“Consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ”</p>
+									<div class="test-info">
+										<img src="images/review-img.jpg" alt="">
+										<div class="test-authot">
+											<h4>Sonata River</h4>
+											<h5>Company ABC</h5>
+										</div>
+									</div>
+
+								</div>
+							</div>
+
+
+							<div class="item">
+								<div class="test-box">
+									<p>“Consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ”</p>
+									<div class="test-info">
+										<img src="images/review-img.jpg" alt="">
+										<div class="test-authot">
+											<h4>Sonata River</h4>
+											<h5>Company ABC</h5>
+										</div>
+									</div>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--	End Testimonial Section-->
+
+
+	<!--Start Logo Section-->
+	<section class="logo-section text-center">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2">
+					<div class="logo-box">
+						<img src="images/logo1.jpg" alt="">
+					</div>
+				</div>
+
+				<div class="col-lg-2">
+					<div class="logo-box">
+						<img src="images/logo2.jpg" alt="">
+					</div>
+				</div>
+
+				<div class="col-lg-2">
+					<div class="logo-box">
+						<img src="images/logo3.jpg" alt="">
+					</div>
+				</div>
+
+				<div class="col-lg-2">
+					<div class="logo-box">
+						<img src="images/logo4.jpg" alt="">
+					</div>
+				</div>
+
+				<div class="col-lg-2">
+					<div class="logo-box">
+						<img src="images/logo5.jpg" alt="">
+					</div>
+				</div>
+
+				<div class="col-lg-2">
+					<div class="logo-box">
+						<img src="images/logo6.jpg" alt="">
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</section>
+	<!--End Logo Section-->
+
+
+
+	<!--Start CTA Section	-->
+	<section class="cta-section">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-9">
+					<div class="cta-text">
+						<h2>Don't hesitate to contact us any time</h2>
+						<p>We provide high standar clean website for your business solutions</p>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="cta-btn">
+						<a href="" class="btn btn-primary">book an appointment</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!--End CTA Section	-->
+
+
+	<!--Start Footer-->
+	<footer>
+		<div class="container" id="contact-us-id">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="footer-col footer-about">
+						<p class="phone-text">+84 988 449 259</p>
+						<p>contact@yourwebsite.com</p>
+						<p>99 S.t Jomblo Park Pekanbaru 28292. Indonesia</p>
+						<ul class="top-social">
+							<li><a href=""><i class="fa fa-facebook"></i></a> </li>
+							<li><a href=""><i class="fa fa-instagram"></i></a> </li>
+							<li><a href=""><i class="fa fa-twitter"></i></a> </li>
+							<li><a href=""><i class="fa fa-pinterest"></i></a> </li>
+						</ul>
+					</div>
+				</div>
+
+
+				<div class="col-lg-3">
+					<div class="footer-col footer-menu">
+						<h3>Useful Links</h3>
+						<ul>
+							<li> <a href="">About Us</a> </li>
+							<li> <a href="">Corporate Profile</a> </li>
+							<li> <a href="">Our Team</a> </li>
+							<li> <a href="">Portfolio</a> </li>
+							<li> <a href="">Our Office</a> </li>
+						</ul>
+					</div>
+				</div>
+
+
+				<div class="col-lg-3">
+					<div class="footer-col footer-menu">
+						<h3>Useful Links</h3>
+						<ul>
+							<li> <a href="">About Us</a> </li>
+							<li> <a href="">Corporate Profile</a> </li>
+							<li> <a href="">Our Team</a> </li>
+							<li> <a href="">Portfolio</a> </li>
+							<li> <a href="">Our Office</a> </li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-3">
+					<div class="footer-col footer-menu">
+						<h3>Useful Links</h3>
+						<ul>
+							<li> <a href="">About Us</a> </li>
+							<li> <a href="">Corporate Profile</a> </li>
+							<li> <a href="">Our Team</a> </li>
+							<li> <a href="">Portfolio</a> </li>
+							<li> <a href="">Our Office</a> </li>
+						</ul>
+					</div>
+				</div>
+
+
+
+				<div class="col-md-12">
+					<div class="copy-text text-center">
+						<p>Copyright 2022 All Right Reserved.</p>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</footer>
+	<!--End Footer-->
+
+
+
+	<!--Script	-->
+	<script src="js/jquery-3.4.1.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+	<script>
+		$('.testimonial-carousel').owlCarousel({
+			loop: true,
+			margin: 10,
+			nav: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 1
+				},
+				1000: {
+					items: 1
+				}
+			}
+		})
+	</script>
+</body>
+
+</html>
