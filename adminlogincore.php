c100c0058a614cb1021f763a0b075e52ef02e47c
zahid hasan <zahidhasan6112@gmail.com>
2022-08-30 11:36:44 -0700

    admin login working

---
+++ adminlogincore.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'dbconnection.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$query = "select * from admin where email='" . $email . "' and password='" . $password . "';";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+        $_SESSION['user'] = "admin";
+        $_SESSION['email'] = $row['email'];
+        header("Location: admin");
+    }
+} else {
+    echo "<script>
+    alert('email or password is incorrect');
+    window.location.href='index.php';
+    </script>";
+}
---
+++ dbconnection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lawyerfirm";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if(mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
